5d9f36b8f670be90d4751a026d4ff3eeb2f3e0c9
Dan Pelensky <dan@pelensky.com>
2016-12-07 15:17:39 +0000

    minus 5

--- jasmine-standalone-2.5 2.2/spec/thermostatSpec.js
+++ jasmine-standalone-2.5 2.2/spec/thermostatSpec.js
@@ -9,19 +9,26 @@ it("should initialize at 20 degrees", function(){
   expect(thermostat.temperature).toEqual(20);
 });
 
-it("should increase temperature by 5", function(){
-  thermostat.increaseTemp();
+it("should increase temperature by the amount specified", function(){
+  thermostat.increaseTemp(5);
   expect(thermostat.temperature).toEqual(25);
 });
 
-it("should decrease temperature by 5", function(){
-  thermostat.decreaseTemp();
+it("should decrease temperature by the amount specified", function(){
+  thermostat.decreaseTemp(5);
   expect(thermostat.temperature).toEqual(15);
 });
 
 it("should have a minimum temperature of 10 degrees", function(){
-  thermostat.decreaseTemp();
-  thermostat.decreaseTemp();
-  expect(function(){ thermostat.decreaseTemp();}).toThrowError('Minimum temperature is 10 degrees');
+  thermostat.decreaseTemp(11)
+  console.log(thermostat.temperature)
+  // expect(function(){ thermostat.decreaseTemp(11);}).toThrowError('Minimum temperature is 10 degrees');
+  expect(thermostat.temperature).toEqual(10);
 });
+
+// it("should have a maximum temperature of 32 degrees if power save is off", function(){
+//   thermostat.increaseTemp();
+//   thermostat.increaseTemp();
+//   expect(function(){thermostat.increaseTemp();}).toThrowError('Maximum temperature is 32 degrees')
+// });
 });
--- jasmine-standalone-2.5 2.2/src/thermostat.js
+++ jasmine-standalone-2.5 2.2/src/thermostat.js
@@ -1,18 +1,22 @@
 function Thermostat() {
-    this.temperature = 20;
+  this.temperature = 20;
 };
 
-Thermostat.prototype.increaseTemp = function(){
-  this.temperature += 5;
+Thermostat.prototype.increaseTemp = function(number){
+  // if(this.temperature <= 32) {
+  this.temperature += number;
+  // } else {
+  //   throw new Error('Maximum temperature is 32 degrees')
+  // }
 };
 
-Thermostat.prototype.decreaseTemp = function() {
-  if (this.temperature > 10) {
-    this.temperature -= 5;
+Thermostat.prototype.decreaseTemp = function(number){
+  this.temperature -= number;
+  if (this.temperature < 10) {
+    this.temperature = 10;
+    return this.temperature;
+    // throw new Error('Minimum temperature is 10 degrees')
   } else {
-    throw new Error('Minimum temperature is 10 degrees')
+    return this.temperature;
   }
-
-
-
 };
