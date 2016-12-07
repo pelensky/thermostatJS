95a3193eb13ae0002ffc01fb387500c4caa97f4a
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2016-12-07 17:04:20 +0000

    created html file but js file didn't pull properly

--- jasmine-standalone-2.5 2.2/src/thermostat.js
+++ jasmine-standalone-2.5 2.2/src/thermostat.js
@@ -1,25 +1,24 @@
 function Thermostat() {
   this.temperature = 20;
   this.isPowerSave = true;
-  this.energyUsage = "medium-usage"
 };
 
 Thermostat.prototype.increaseTemp = function(number){
-    this.temperature += number;
+  this.temperature += number;
   if(this.isPowerSave) {
-      if (this.temperture < 26) {
+    if (this.temperture < 26) {
       return this.temperature
-      } else {
+    } else {
       this.temperature = 25
       return this.temperature
-      }
+    }
   } else {
     if (this.temperture < 33) {
-    return this.temperature
+      return this.temperature
     } else {
-    this.temperature = 32
-    return this.temperature
-  }
+      this.temperature = 32
+      return this.temperature
+    }
   }
 };
 
@@ -30,22 +29,5 @@ Thermostat.prototype.decreaseTemp = function(number){
     return this.temperature;
   } else {
     return this.temperature;
-  }
-};
-
-Thermostat.prototype.reset = function(){
-  this.temperature = 20;
-  return this.temperature;
-};
-
-Thermostat.prototype.energy = function(){
-  if(this.temperature < 18) {
-    this.energyUsage = "low-usage";
-    return this.energyUsage;
-  } else if (this.temperature >25) {
-      this.energyUsage = "high-usage";
-      return this.energyUsage;
-  } else {
-    return this.energyUsage;
   }
 };
