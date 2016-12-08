b42a2f35f94cd7425b11614d56406a10ef42b869
TudorTacal <tudortacal@gmail.com>
2016-12-08 10:59:05 +0000

    Minimum temperature is 10 degrees

--- jasmine/jasmine-standalone-2.5.2/spec/thermostatSpec.js
+++ jasmine/jasmine-standalone-2.5.2/spec/thermostatSpec.js
@@ -3,12 +3,31 @@ describe("Thermostat", function () {
   beforeEach(function () {
     thermostat = new Thermostat();
   });
+
   it('initializes at 20 degrees', function () {
     expect(thermostat.getTemperature()).toEqual(20);
   });
+
   it('increases temperature by one degree with an up function', function () {
     thermostat.increaseTemperature();
     expect(thermostat.getTemperature()).toEqual(21);
+  });
+
+  it('descreases temperature by 1 degree', function () {
+    thermostat.decreaseTemperature();
+    expect(thermostat.getTemperature()).toEqual(19);
+  });
+
+  it('minimum temperature is 10 degrees', function () {
+    var i = 0;
+    do {
+      thermostat.decreaseTemperature();
+      i++;
+      console.log(thermostat.getTemperature());
+    }
+    while (i < 11);
+    alert(thermostat.getTemperature());
+    expect(function () {thermostat.decreaseTemperature()}).toThrow("Mininum temperature is 10 degrees. Cannot go below.")
   });
 
 });
--- jasmine/jasmine-standalone-2.5.2/src/thermostat.js
+++ jasmine/jasmine-standalone-2.5.2/src/thermostat.js
@@ -9,3 +9,8 @@ Thermostat.prototype.getTemperature = function () {
 Thermostat.prototype.increaseTemperature = function () {
   this.temperature += 1;
 };
+
+Thermostat.prototype.decreaseTemperature = function () {
+  if (this.temperature <= 10) throw "Mininum temperature is 10 degrees. Cannot go below."
+  this.temperature -= 1;
+};
