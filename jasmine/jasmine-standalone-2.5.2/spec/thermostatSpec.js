4703cdcce378bd4c36f16a4dd185fc8f3703ad93
Dan Pelensky <dan@pelensky.com>
2016-12-08 12:28:23 +0000

    html created

--- jasmine/jasmine-standalone-2.5.2/spec/thermostatSpec.js
+++ jasmine/jasmine-standalone-2.5.2/spec/thermostatSpec.js
@@ -8,56 +8,65 @@ describe("Thermostat", function () {
     expect(thermostat.getTemperature()).toEqual(20);
   });
 
-  it('increases temperature by one degree with an up function', function () {
-    thermostat.increaseTemperature();
-    expect(thermostat.getTemperature()).toEqual(21);
-  });
+  describe("change temperature", function () {
 
-  it('descreases temperature by 1 degree', function () {
-    thermostat.decreaseTemperature();
-    expect(thermostat.getTemperature()).toEqual(19);
-  });
+    it('increases temperature by one degree with an up function', function () {
+      thermostat.increaseTemperature();
+      expect(thermostat.getTemperature()).toEqual(21);
+    });
 
-  it('minimum temperature is 10 degrees', function () {
-    var i = 0;
-    do {
+    it('descreases temperature by 1 degree', function () {
       thermostat.decreaseTemperature();
-      i++;
-    }
-    while (i < 10);
-    expect(function () {thermostat.decreaseTemperature()}).toThrow("Mininum temperature is 10 degrees. Cannot go below.")
+      expect(thermostat.getTemperature()).toEqual(19);
+    });
   });
 
-  it('if power saving mode is on the maximum temperature is 25 degrees', function () {
-    thermostat.powerSaving('on');
-    var i = 0;
-    do {
+  describe("Minimum and maximum", function () {
+
+    it('minimum temperature is 10 degrees', function () {
+      var i = 0;
+      do {
+        thermostat.decreaseTemperature();
+        i++;
+      }
+      while (i < 10);
+      expect(function () {thermostat.decreaseTemperature()}).toThrow("Mininum temperature is 10 degrees. Cannot go below.")
+    });
+
+    it('if power saving mode is on the maximum temperature is 25 degrees', function () {
+      thermostat.powerSaving('on');
+      var i = 0;
+      do {
+        thermostat.increaseTemperature();
+        i++;
+      }
+      while (i < 6);
+      expect(function() {thermostat.increaseTemperature()}).toThrow("Maximum temperature is 25 degrees. Cannot go above.")
+    });
+
+    it('if power saving mode is off the maximum temperature is 32 degrees', function () {
+      thermostat.powerSaving('off');
+      var i = 0;
+      do {
+        thermostat.increaseTemperature();
+        i++;
+      }
+      while (i < 13);
+      expect(function(){thermostat.increaseTemperature()}).toThrow("Maximum temperature is 32 degrees. Cannot go above.")
+    });
+
+    it('maximum temperature should be 25 by default - power saving mode', function (){
+      expect(thermostat.maximumTemperature).toEqual(25);
+    });
+  });
+
+  describe("reset", function () {
+
+    it('resets the temperature to 20', function () {
       thermostat.increaseTemperature();
-      i++;
-    }
-    while (i < 6);
-    expect(function() {thermostat.increaseTemperature()}).toThrow("Maximum temperature is 25 degrees. Cannot go above.")
-  });
-
-  it('if power saving mode is off the maximum temperature is 32 degrees', function () {
-    thermostat.powerSaving('off');
-    var i = 0;
-    do {
-      thermostat.increaseTemperature();
-      i++;
-    }
-    while (i < 13);
-    expect(function(){thermostat.increaseTemperature()}).toThrow("Maximum temperature is 32 degrees. Cannot go above.")
-  });
-
-  it('maximum temperature should be 25 by default - power saving mode', function (){
-    expect(thermostat.maximumTemperature).toEqual(25);
-  });
-
-  it('resets the temperature to 20', function () {
-    thermostat.increaseTemperature();
-    thermostat.reset();
-    expect(thermostat.getTemperature()).toEqual(20);
+      thermostat.reset();
+      expect(thermostat.getTemperature()).toEqual(20);
+    });
   });
 
   describe("energy usage", function () {
