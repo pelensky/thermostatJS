375eeff5055e504eedcc38060d691b5ee1bb0a7a
Dan Pelensky <dan@pelensky.com>
2016-12-08 17:22:50 +0000

    added current weather

--- jasmine/jasmine-standalone-2.5.2/src/interactive.js
+++ jasmine/jasmine-standalone-2.5.2/src/interactive.js
@@ -1,19 +1,8 @@
 $(document).ready(function() {
-  var jsonData;
-  $.ajax ({
-    url: 'http://api.openweathermap.org/data/2.5/forecast/city?id=2643743&APPID={6d4bbb8a1db58900a6e66af4b3cdacca}',
-    jsonp: "callback",
-    dataType: "jsnop",
-    data: {
-      q: 'city',
-      format: "json"
-    },
-    success: function(response) {
-      jsonData = response;
-      console.log(response);
-    }
-  });
 
+$.get('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=618b0fb946dbbf8f875877fbac622c50&units=metric', function(data){
+  $('#weather').text(data.main.temp)
+});
   var thermostat = new Thermostat();
 
   $("#powerSaveOn").on("click", function(){
