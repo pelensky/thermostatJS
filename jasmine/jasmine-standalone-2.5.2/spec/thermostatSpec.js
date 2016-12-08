74ef4557c9d3ce75712bc36ab78cc28ff299926a
TudorTacal <tudortacal@gmail.com>
2016-12-08 09:57:54 +0000

    Created a thermostat with a temperature of 20 degrees.

---
+++ jasmine/jasmine-standalone-2.5.2/spec/thermostatSpec.js
@@ -0,0 +1,9 @@
+describe("Thermostat", function () {
+  var thermostat;
+  beforeEach(function () {
+    thermostat = new Thermostat();
+  });
+  it('initializes at 20 degrees', function () {
+    expect(thermostat.temperature()).toEqual(20);
+  });
+});
