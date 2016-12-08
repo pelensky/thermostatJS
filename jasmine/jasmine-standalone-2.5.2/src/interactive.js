cb3965547cdcb9f09cd786872547cec686990ab9
Dan Pelensky <dan@pelensky.com>
2016-12-08 16:14:13 +0000

    refactored

--- jasmine/jasmine-standalone-2.5.2/src/interactive.js
+++ jasmine/jasmine-standalone-2.5.2/src/interactive.js
@@ -2,47 +2,34 @@ $(document).ready(function() {
 
   var thermostat = new Thermostat();
 
-  $("#powerSavingMode").text("Thermostat is on");
-
   $("#powerSaveOn").on("click", function(){
     thermostat.powerSaving("on");
-    $("#powerSavingMode").text("Thermostat is on");
-    console.log(thermostat.maximumTemperature);
+    $("#powerSavingMode").text("Power Saving Mode is on");
   });
 
   $("#powerSaveOff").click(function (){
     thermostat.powerSaving("off");
-    $("#powerSavingMode").text("Thermostat is off");
-    console.log(thermostat.maximumTemperature);
+    $("#powerSavingMode").text("Power Saving Mode is off");
   });
 
   $('#increase').click(function(){
     thermostat.increaseTemperature();
-      $("#temperature").text(thermostat.getTemperature());
-      $('#usage').text(thermostat.energyUsage());
-      if (thermostat.energyUsage() === 'Low usage') $('#usage').css('color', 'green');
-      if (thermostat.energyUsage() === 'Medium usage') $('#usage').css('color', 'black');
-      if (thermostat.energyUsage() === 'High usage') $('#usage').css('color', 'red');
-
   });
 
   $('#decrease').click(function(){
     thermostat.decreaseTemperature();
+  });
+
+  $('#reset').click(function () {
+    thermostat.reset();
+
+  });
+
+  $(':button').click(function () {
     $("#temperature").text(thermostat.getTemperature());
     $('#usage').text(thermostat.energyUsage());
     if (thermostat.energyUsage() === 'Low usage') $('#usage').css('color', 'green');
     if (thermostat.energyUsage() === 'Medium usage') $('#usage').css('color', 'black');
     if (thermostat.energyUsage() === 'High usage') $('#usage').css('color', 'red');
   });
-
-  $('#reset').click(function () {
-    thermostat.reset();
-    $("#temperature").text(thermostat.getTemperature());
-    $('#usage').text(thermostat.energyUsage());
-    if (thermostat.energyUsage() === 'Low usage') $('#usage').css('color', 'green');
-    if (thermostat.energyUsage() === 'Medium usage') $('#usage').css('color', 'black');
-    if (thermostat.energyUsage() === 'High usage') $('#usage').css('color', 'red');  
   });
-
-
-});
